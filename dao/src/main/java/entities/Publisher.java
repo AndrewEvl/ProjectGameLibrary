f1f7b6ae628d8afe8b1726404ee3f963d4939306
AndrewEvl <Lino220037@gmail.com>
2017-06-01 16:14:11 +0300

    Add entity test

--- dao/src/main/java/entities/Publisher.java
+++ dao/src/main/java/entities/Publisher.java
@@ -17,7 +17,7 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     @Setter
-    private long id;
+    private Long id;
     @Column (name = "name")
     @Setter
     @Getter
--- dao/src/test/java/entities/EntityTest.java
+++ dao/src/test/java/entities/EntityTest.java
@@ -1,10 +1,16 @@
 package entities;
 
+import entities.reviews.ReviewGame;
+import entities.systemParts.Cpu;
 import org.hibernate.Session;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.*;
+
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -25,6 +31,7 @@ public class EntityTest {
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
         Developer developer = new Developer();
         developer.setName("Test");
@@ -33,36 +40,57 @@ public class EntityTest {
         assertEquals(developer.getName(), "Test");
         assertEquals(developer.getId(), 1);
 
+        transaction.commit();
         session.close();
     }
 
-//    @Test
-//    public void testSaveGame() {
-//        Session session = SESSION_FACTORY.openSession();
-//
-//        Game game = new Game();
-//        game.setId(1L);
-//        game.setName("test");
-//        assertEquals(game.getId(), 1);
-//        assertEquals(game.getName(), "test");
-//
-//        session.close();
-//
-//    }
-//
-//    @Test
-//    public void testSaveGenre() {
-//        Session session = SESSION_FACTORY.openSession();
-//
-//        Genre genre = new Genre();
-//        genre.setId(1L);
-//        genre.setName("test");
-//        assertEquals(genre.getId(), 1);
-//        assertEquals(genre.getName(), "test");
-//
-//        session.close();
-//    }
-//
+    @Test
+    public void testSaveGame() {
+        Session session = SESSION_FACTORY.openSession();
+
+        Game game = new Game();
+        Genre genre = new Genre();
+        Publisher publisher = new Publisher();
+        Developer developer = new Developer();
+        ReviewGame reviewGame = new ReviewGame();
+        SystemSetting systemSetting = new SystemSetting();
+        reviewGame.setText("Test");
+        reviewGame.setGame(game);
+        publisher.setName("Test");
+        genre.setName("Test");
+        developer.setName("Test");
+        session.save(systemSetting);
+        session.save(reviewGame);
+        session.save(developer);
+        session.save(publisher);
+        session.save(genre);
+        game.setName("test");
+        game.setGenre(genre);
+        game.setDeveloper(developer);
+        game.setPublisher(publisher);
+//        game.setSystemSetting((Set<SystemSetting>) systemSetting);
+//        game.setReviewGame((Set<ReviewGame>) reviewGame);
+        session.save(game);
+
+
+        session.close();
+
+    }
+
+    @Test
+    public void testSaveGenre() {
+        Session session = SESSION_FACTORY.openSession();
+
+        Genre genre = new Genre();
+        genre.setId(1L);
+        genre.setName("test");
+        assertEquals(genre.getId(), 1);
+        assertEquals(genre.getName(), "test");
+
+        session.close();
+    }
+
+    //
 ////    @Test
 ////    public void testSaveNews() {
 ////        Session session = SESSION_FACTORY.openSession();
@@ -78,52 +106,48 @@ public class EntityTest {
 ////        session.close();
 ////    }
 //
-//    @Test
-//    public void testSavePublisher() {
-//        Session session = SESSION_FACTORY.openSession();
-//
-//        Publisher publisher = new Publisher();
-//        publisher.setId(1L);
-//        publisher.setName("test");
-//
+    @Test
+    public void testSavePublisher() {
+        Session session = SESSION_FACTORY.openSession();
+
+        Publisher publisher = new Publisher();
+        publisher.setId(1L);
+        publisher.setName("test");
+
 //        assertEquals(publisher.getId(), 1);
 //        assertEquals(publisher.getName(), "test");
-//
-//
-//        session.close();
-//    }
-//
-//    @Test
-//    public void testSavePlatform() {
-//        Session session = SESSION_FACTORY.openSession();
-//
-//        Platform platform = new Platform();
-//        platform.setId(1L);
-//        platform.setName("test");
-//
-//        assertEquals(platform.getId(), 1);
-//        assertEquals(platform.getName(), "test");
-//
-//
-//        session.close();
-//    }
-//
-////    @Test
-////    public void testSaveReview() {
-////        Session session = SESSION_FACTORY.openSession();
-////
-////        ReviewGame reviewGames = new ReviewGame();
-////        review.setId(1L);
-////        review.setText("test");
-////
-////        assertEquals(review.getId(), 1);
-////        assertEquals(review.getText(), "test");
-////
-////
-////        session.close();
-////    }
-//
-//
+
+        session.close();
+    }
+
+    @Test
+    public void testSavePlatform() {
+        Session session = SESSION_FACTORY.openSession();
+
+        Platform platform = new Platform();
+        platform.setId(1L);
+        platform.setName("test");
+
+        assertEquals(platform.getId(), 1);
+        assertEquals(platform.getName(), "test");
+
+        session.close();
+    }
+
+    @Test
+    public void testSaveReview() {
+        Session session = SESSION_FACTORY.openSession();
+
+        ReviewGame reviewGames = new ReviewGame();
+        reviewGames.setId(1L);
+        reviewGames.setText("test");
+
+//        assertEquals(reviewGames.getId(),1L);
+//        assertEquals(reviewGames.getText(), "test");
+
+        session.close();
+    }
+
     @AfterClass
     public static void finish() {
         SESSION_FACTORY.close();
