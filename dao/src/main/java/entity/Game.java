4118c9498d0246b20125dcf9f42a677cd394a9c7
AndrewEvl <Lino220037@gmail.com>
2017-06-09 18:12:32 +0300

    add querydsl

--- dao/src/main/java/entity/Game.java
+++ dao/src/main/java/entity/Game.java
@@ -16,7 +16,6 @@ import java.util.Set;
 @ToString
 @EqualsAndHashCode(exclude = "platform")
 @Inheritance(strategy = InheritanceType.JOINED)
-@MappedSuperclass
 @NoArgsConstructor
 @Table(name = "games")
 public class Game {
