16614ae63a99a3857bb6d82371de0bca875877bd
AndrewEvl <Lino220037@gmail.com>
2017-06-14 20:37:55 +0300

    add Spring

--- dao/src/main/java/dao/GameDao.java
+++ dao/src/main/java/dao/GameDao.java
@@ -1,6 +1,11 @@
 package dao;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import entity.Game;
+import entity.QGame;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Created by User on 25.05.2017.
@@ -10,4 +15,17 @@ public class GameDao extends BaseDao<Game> {
     public GameDao() {
         super(Game.class);
     }
+
+//    public Game fullInfo (Game game) {
+//        QGame qGame = new QGame("myGame");
+//        JPAQuery<Game> query = new JPAQuery<>();
+//        query.select(qGame.name,
+//                qGame.developer,
+//                qGame.genre,
+//                qGame.publisher,
+//                qGame.releaseDay)
+//                .from(qGame)
+//                .where(qGame.eq(game));
+//        return query.fetchOne();
+//    }
 }
---
+++ dao/src/test/java/dao/GameDaoTest.java
@@ -0,0 +1,22 @@
+package dao;
+
+import entity.Game;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 14.06.2017.
+ */
+public class GameDaoTest {
+
+//    @Test
+//    public void fullInfo() throws Exception {
+//        GameDao gameDao = new GameDao();
+//        Game game = new Game();
+//        game.setName("test");
+//        Game fullInfo = gameDao.fullInfo(game);
+//        System.out.println(fullInfo);
+//    }
+
+}
