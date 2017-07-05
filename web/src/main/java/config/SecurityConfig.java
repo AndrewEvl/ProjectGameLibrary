0b6792bbf92bb7eba82b6e44f567f145a3d3dc9f
AndrewEvl <Lino220037@gmail.com>
2017-07-05 21:15:11 +0300

    Spring Security

--- web/src/main/java/config/SecurityConfig.java
+++ web/src/main/java/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -56,11 +57,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .permitAll()
                 .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
                 .and()
                 .logout()
-                .logoutUrl("/exit");
+                .logoutUrl("/exit")
+                .and().csrf().disable();
         http.userDetailsService(userDetailsService);
     }
 
